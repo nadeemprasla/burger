ca1f9f353eb2c491448585344b4fcb2e6bc24d7d
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-11-26 22:53:50 -0600

    added local too

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,17 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: process.argv[2],
+        database: 'burgers_db'
+    })
+};
 
 connection.connect((err) => {
     if (err) {
