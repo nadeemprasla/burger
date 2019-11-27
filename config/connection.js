b84922a3080aa805ad4bbe54a55dc4443d01b605
Nadeem Prasla <nadeemprasla@yahoo.com>
2019-11-26 22:51:27 -0600

    fixed connection

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,6 @@
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
-    host: "k9xdebw4k3zynl4u.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    port: 3306,
-    user: "l0h7kn0h4buf38gd",
-    password: "xaaovdp0xx5c8r2e",
-    database: "n8fv5v930e7e540m"
-});
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect((err) => {
     if (err) {
